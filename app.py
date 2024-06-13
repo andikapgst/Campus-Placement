0297cc294b623430cfcf13fca36168183ba2f894
Andika Pangestu <168011780+andikapgst@users.noreply.github.com>
2024-06-13 12:39:14 +0700

    updated

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import pickle
 import streamlit as st
-import pandas as pd
 
 # Membaca model
 model = pickle.load(open('model.pkl', 'rb'))
@@ -8,7 +7,7 @@ model = pickle.load(open('model.pkl', 'rb'))
 # Judul web
 st.title('Prediksi Penempatan Kerja')
 
-# Link untuk kembali sebagai tombol dengan latar belakang warna biru dan teks putih
+# Link untuk kembali sebagai tombol
 st.markdown("""
     <style>
     .button {
@@ -23,8 +22,25 @@ st.markdown("""
         padding: 0.25em 0.75em;
     }
     </style>
-    <a href="https://undira.netlify.app/" class="button">Kembali ke halaman utama</a>
-""", unsafe_allow_html=True)
+    <a href="https://undira.netlify.app/" class="button">Kembali ke halaman utama</a>""", unsafe_allow_html=True)
+
+# Inisialisasi session state
+if 'Gender' not in st.session_state:
+    st.session_state['Gender'] = ''
+if 'Nilai SMP' not in st.session_state:
+    st.session_state['Nilai SMP'] = ''
+if 'Nilai SMA' not in st.session_state:
+    st.session_state['Nilai SMA'] = ''
+if 'Nilai Sarjana' not in st.session_state:
+    st.session_state['Nilai Sarjana'] = ''
+if 'Pengalaman Kerja' not in st.session_state:
+    st.session_state['Pengalaman Kerja'] = ''
+if 'Nilai Tes Kelayakan' not in st.session_state:
+    st.session_state['Nilai Tes Kelayakan'] = ''
+if 'Spesialisasi Magang' not in st.session_state:
+    st.session_state['Spesialisasi Magang'] = ''
+if 'Nilai Magang' not in st.session_state:
+    st.session_state['Nilai Magang'] = ''
 
 # Kolom
 col1, col2 = st.columns(2)
@@ -33,27 +49,29 @@ with col1:
     gender_options = ['Laki-laki', 'Perempuan']
     Gender = st.radio('Gender', gender_options)
     gender_numeric = 1 if Gender == 'Laki-laki' else 2
-    NilaiSMP = st.text_input('Input Nilai SMP')
-    NilaiSMA = st.text_input('Input Nilai SMA')
-    NilaiSarjana = st.text_input('Input Nilai Sarjana')
+    NilaiSMP = st.text_input('Input Nilai SMP', key='Nilai SMP')
+    NilaiSMA = st.text_input('Input Nilai SMA', key='Nilai SMA')
+    NilaiSarjana = st.text_input('Input Nilai Sarjana', key='Nilai Sarjana')
+
 with col2:
     workex_options = ['Tidak', 'Ya']
     PengalamanKerja = st.radio('Pengalaman Kerja', workex_options)
     pengalamankerja_numeric = 1 if PengalamanKerja == 'Tidak' else 2
-    NilaiTesKelayakan = st.text_input('Input Nilai Tes Kelayakan')
+    NilaiTesKelayakan = st.text_input('Input Nilai Tes Kelayakan', key='Nilai Tes Kelayakan')
     spesialisasi_options = ['Pilih Spesialiasi', 'Pengabdian Masyarakat', 'Industri Bisnis']
     SpesialisasiMagang = st.selectbox('Spesialisasi Magang', spesialisasi_options)
     spesialisasi_numeric = 1 if SpesialisasiMagang == 'Pengabdian Masyarakat' else 2 if SpesialisasiMagang == 'Industri Bisnis' else 0
-    NilaiMagang = st.text_input('Input Nilai Magang')
+    NilaiMagang = st.text_input('Input Nilai Magang', key='Nilai Magang')
 
 # Button Prediksi
 if st.button('Prediksi'):
     # Validasi input
     try:
-        inputs = [gender_numeric, float(NilaiSMP), float(NilaiSMA), float(NilaiSarjana), pengalamankerja_numeric, float(NilaiTesKelayakan), spesialisasi_numeric, float(NilaiMagang)]
+        df_input = [gender_numeric, float(NilaiSMP), float(NilaiSMA), float(NilaiSarjana), pengalamankerja_numeric, 
+                  float(NilaiTesKelayakan), spesialisasi_numeric, float(NilaiMagang)]
         
-        if all(inputs):
-            prediction = model.predict([inputs])
+        if all(df_input):
+            prediction = model.predict([df_input])
             if prediction[0] == 1:
                 st.success('Selamat, Anda berpeluang untuk mendapatkan penempatan kerja.')
             else:
